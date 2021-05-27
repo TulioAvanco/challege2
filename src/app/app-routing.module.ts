8a26706f3ff8e0945ace7d00428ab03dcc442bdb
TulioAvanco <tulioavanco12@gmail.com>
2021-05-27 19:02:29 -0300

    upload components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./app/component/views/home/home.component";
+import {RepositoriesComponent} from "./app/component/views/repositories/repositories.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component: HomeComponent},
+  {
+    path:'repositories',
+    component: RepositoriesComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {NavigationComponent} from './app/component/views/navigation/navigation.component';
+import {HomeComponent} from './app/component/views/home/home.component';
+import {RepositoriesComponent} from './app/component/views/repositories/repositories.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {TableComponent} from './app/component/views/table/table.component';
+import {MatInputModule} from "@angular/material/input";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    RepositoriesComponent,
+    TableComponent,
+
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
